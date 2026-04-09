4163b33ecd3a1b20b84e44125d0bfcdcc49ee71e
fernandacf1000-star <fernandacf1000@gmail.com>
2026-04-09 20:01:54 -0300

    Update useAuth.ts

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { supabase } from './supabase'
+import { supabase } from '../lib/supabase'
 import type { Session, User } from '@supabase/supabase-js'
 
 export type AuthState =
