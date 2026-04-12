7337b49dd5941a0085c7307fb512041bf367e80a
fernandacf1000-star <fernandacf1000@gmail.com>
2026-04-12 19:32:25 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
